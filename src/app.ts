b24cd90b23d911b53ef9a994f4771b310e2bf612
shreyam-dharmvans <sdharmvans@gmail.com>
2024-08-01 14:10:10 +0530

    changed cors origin

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ async function main() {
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173/", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 //app.use(morgan("dev"));
 
 
